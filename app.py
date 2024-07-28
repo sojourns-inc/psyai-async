c5bb4239b09aa41106a7afc2b35983502d81b4a0
Oleksiy Levkovskyy <lex@sojourns.io>
2024-07-28 09:49:49 -0400

    Update model endpoint parameter

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import falcon
 from falcon import asgi
 from supabase import create_client
 from openai import OpenAI
+import base64
 import google.generativeai as genai
 from dotenv import load_dotenv
 
@@ -10,17 +11,18 @@ load_dotenv()
 
 
 class AppConfig:
-    SUPABASE_URL = os.environ.get("SUPABASE_URL")
-    SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
-    USER_ID = os.environ.get("USER_ID")
-    LLM_SYSTEM = os.environ.get("LLM_SYSTEM")
-    LLM_SUFFIX = os.environ.get("LLM_SUFFIX")
-    LLM_SUFFIX_BETA = os.environ.get("LLM_SUFFIX_BETA")
-    EXPERIMENTAL_BASE_URL = os.environ.get("EXPERIMENTAL_BASE_URL")
-    EXPERIMENTAL_API_KEY = os.environ.get("EXPERIMENTAL_API_KEY")
-    EXPERIMENTAL_MODEL_NAME = os.environ.get("EXPERIMENTAL_MODEL_NAME")
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    USER_ID = os.getenv("USER_ID")
+    LLM_SYSTEM = os.getenv("LLM_SYSTEM")
+    LLM_SUFFIX = os.getenv("LLM_SUFFIX")
+    LLM_SUFFIX_BETA = os.getenv("LLM_SUFFIX_BETA")
+    EXPERIMENTAL_BASE_URL = os.getenv("EXPERIMENTAL_BASE_URL")
+    EXPERIMENTAL_API_KEY = os.getenv("EXPERIMENTAL_API_KEY")
+    EXPERIMENTAL_MODEL_NAME = os.getenv("EXPERIMENTAL_MODEL_NAME")
+    LLM_HUMOROUS_PERSONA = base64.b64decode(os.getenv("LLM_HUMOROUS_PERSONA")).decode("utf-8")
 
 
 def initialize_genai_model():
@@ -52,29 +54,28 @@ class PromptResource:
             AppConfig.SUPABASE_URL, AppConfig.SUPABASE_KEY
         )
         self.openai_client = OpenAI(api_key=AppConfig.OPENAI_API_KEY)
-        self.experimental_client = OpenAI(base_url=AppConfig.EXPERIMENTAL_BASE_URL, api_key=AppConfig.EXPERIMENTAL_API_KEY)
+        self.experimental_client = OpenAI(
+            base_url=AppConfig.EXPERIMENTAL_BASE_URL,
+            api_key=AppConfig.EXPERIMENTAL_API_KEY,
+        )
         self.gemini_model = initialize_genai_model()
 
     async def on_post(self, req, resp):
         try:
             payload = await req.media
             query = payload.get("question", "")
+            addl_context = payload.get("context", "")
             temperature = payload.get("temperature", 0)
             tokens = payload.get("tokens", 0)
-            model = req.params.get(
-                "model", "gemini"
-            )  # Default to Gemini if not provided
+            model = req.params.get("model", "gemini")
 
             query_embedding = self._get_embedding(query)
             similar_documents = self._fetch_similar_documents(query_embedding)
             context = self._format_context(similar_documents)
 
-            if model == "openai":
-                response = self._generate_openai_response(query, context, temperature=temperature, tokens=tokens)
-            elif model == "experimental":
-                response = self._generate_experimental_response(query, context, temperature=temperature, tokens=tokens)
-            else:
-                response = self._generate_gemini_response(query, context)
+            response = self._generate_response(
+                query, context, temperature, tokens, model, addl_context
+            )
 
             resp.media = {"assistant": response}
             resp.status = falcon.HTTP_200
@@ -114,23 +115,52 @@ class PromptResource:
             context += f"Content: {doc['content']}\n\n##########\n{doc['metadata']['file_name']}\n##########\n\n\n\n"
         return context
 
-    def _generate_openai_response(self, query, context, temperature=0.2, tokens=3000):
+    def _generate_response(
+        self, query, context, temperature, tokens, model, addl_context
+    ):
+        if model == "openai":
+            return self._generate_openai_response(query, context, temperature, tokens)
+        elif model == "experimental":
+            return self._generate_experimental_response(
+                query, context, temperature, tokens
+            )
+        elif model == "fun":
+            return self._generate_openai_response(
+                query, context, temperature, tokens, fun=True, add_context=addl_context
+            )
+        else:
+            return self._generate_gemini_response(query, context)
+
+    def _generate_openai_response(
+        self, query, context, temperature=0.2, tokens=3000, fun=None, add_context=None
+    ):
         try:
+            actual_context = (
+                f"Here is the chat context:\n\n{add_context if fun else context}"
+            )
+            actual_query = (
+                query
+                if fun
+                else f"Check your context and find out: {query}\n\n{AppConfig.LLM_SUFFIX}"
+            )
             response = self.openai_client.chat.completions.create(
-                model="gpt-4o",
-                temperature=temperature,
-                max_tokens=tokens,
-                top_p=1,
+                model=("gpt-4o" if fun else "gpt-4o"),
+                temperature=1.1 if fun else temperature,
+                frequency_penalty=0.9 if fun else 0.3,
+                presence_penalty=1.0 if fun else 0.0,
+                max_tokens=3000 if fun else tokens,
+                top_p=1 if fun else 1,
                 messages=[
-                    {"role": "system", "content": AppConfig.LLM_SYSTEM},
                     {
-                        "role": "user",
-                        "content": f"Here is the chat context:\n\n{context}",
+                        "role": "system",
+                        "content": (
+                            AppConfig.LLM_HUMOROUS_PERSONA
+                            if fun
+                            else AppConfig.LLM_SYSTEM
+                        ),
                     },
-                    {
-                        "role": "user",
-                        "content": f"Check your context and find out: {query}\n\n{AppConfig.LLM_SUFFIX}",
-                    },
+                    {"role": "user", "content": actual_context},
+                    {"role": "user", "content": actual_query},
                 ],
             )
             return response.choices[0].message.content
@@ -144,8 +174,8 @@ class PromptResource:
                 history=[
                     {
                         "role": "user",
-                        "parts": [f"""Here is the chat context:\n\n{context}"""],
-                    },
+                        "parts": [f"Here is the chat context:\n\n{context}"],
+                    }
                 ]
             )
             convo.send_message(
@@ -156,7 +186,9 @@ class PromptResource:
             print(f"Error generating Gemini response: {str(e)}")
             raise
 
-    def _generate_experimental_response(self, query, context, temperature=0.2, tokens=3000):
+    def _generate_experimental_response(
+        self, query, context, temperature=0.2, tokens=3000
+    ):
         try:
             completion = self.experimental_client.chat.completions.create(
                 model=AppConfig.EXPERIMENTAL_MODEL_NAME,
@@ -178,6 +210,7 @@ class PromptResource:
         except Exception as e:
             print(f"Error generating experimental response: {str(e)}")
             raise
+
 
 app = asgi.App()
 app.add_route("/prompt", PromptResource())
