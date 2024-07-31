edea143c2216a3ecbac7a843bf0d395285b8a63d
Oleksiy Levkovskyy <lex@sojourns.io>
2024-07-30 23:04:58 -0400

    Do something for heroku

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from app import app
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
