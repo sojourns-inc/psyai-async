3f6cbacac38ce6331c107079d26cd33a235a9d89
Oleksiy Levkovskyy <lex@sojourns.io>
2024-07-30 23:14:12 -0400

    Fix port binding for Heroku

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host='0.0.0.0', port=port, reload=True)
